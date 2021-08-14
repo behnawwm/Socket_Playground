df5579ff4b7adb0dd8edca51c58778e48b25367b
Behnam Banaei <61078796+behnawwm@users.noreply.github.com>
2021-08-14 09:49:51 +0430

    initial

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+var clients = 0;
+
+io.on("connection", function (socket) {
+  clients++;
+  socket.emit("newclientconnect", { description: "Hey, welcome!" });
+  socket.broadcast.emit("newclientconnect", {
+    description: clients + " clients connected!",
+  });
+  socket.on("disconnect", function () {
+    clients--;
+    socket.broadcast.emit("newclientconnect", {
+      description: clients + " clients connected!",
+    });
+  });
+});
+http.listen(3000, function () {
+  console.log("listening on localhost:3000");
+});
